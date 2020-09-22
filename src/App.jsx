65815f5cbb5e439547faf715060edd37476f9e3a
Josh Mann <jlmann72@gmail.com>
2020-09-22 17:14:18 -0400

    Basic slider color changes working

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,53 @@
 import React, { Component } from 'react'
-import { Slider } from './components/Slider'
 
 class App extends Component {
+  state = {
+    hue: 90,
+    saturation: 50,
+    lightness: 50,
+  }
+
+  handleHueChange = event => {
+    this.setState({ hue: event.target.value })
+  }
+  handleSaturationChange = event => {
+    this.setState({ saturation: event.target.value })
+  }
+  handleLightnessChange = event => {
+    this.setState({ lightness: event.target.value })
+  }
+
   render() {
     return (
-      <div>
-        <Slider id="hue" sliderMax="360" />
-        <Slider id="saturation" sliderMax="100" />
-        <Slider id="lightness" sliderMax="100" />
+      <div
+        style={{
+          backgroundColor: `hsl(${this.state.hue},${this.state.saturation}%,${this.state.lightness}%)`,
+        }}
+      >
+        <div>
+          <input
+            type="range"
+            value={this.state.hue}
+            max="360"
+            onChange={this.handleHueChange}
+          />
+        </div>
+        <div>
+          <input
+            type="range"
+            value={this.state.saturation}
+            max="100"
+            onChange={this.handleSaturationChange}
+          />
+        </div>
+        <div>
+          <input
+            type="range"
+            value={this.state.lightness}
+            max="100"
+            onChange={this.handleLightnessChange}
+          />
+        </div>
       </div>
     )
   }
